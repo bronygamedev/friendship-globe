6108a6cdf55629e03f37ea5c1d4a33e48786edf7
alex <arandomebrony+git@gmail.com>
2023-08-16 08:52:54 -0500

    converted to godot 3

--- client/proto_type.gd
+++ client/proto_type.gd
@@ -1,9 +1,5 @@
+@tool
 extends Node3D
-@export var earth:MeshInstance3D
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-	
-	
+@export var earth:Node
 func _process(delta):
 	earth.rotate_y(.01)
